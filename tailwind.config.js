2a6ae9751d3196896c0b73c1641338b52241146a
nmanumr <nmanumr@gmail.com>
2021-09-01 21:58:42 +0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    mode: 'jit',
+    purge: [
+        './public/**/*.html',
+        './src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}'
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                'recoleta': ['Recoleta', 'ui-serif', 'Georgia', 'Cambria', "Times New Roman", 'Times', 'serif'],
+                'sans': ['IBM Plex Sans', 'ui-sans-serif', 'sans-serif']
+            }
+        },
+    },
+    plugins: [
+        require('@tailwindcss/typography'),
+    ]
+};
