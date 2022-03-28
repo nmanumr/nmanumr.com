093d30702ae1feb98953385ba6846ecf234a9df4
nmanumr <nmanumr@gmail.com>
2022-03-28 14:59:34 +0500

    updates to astro 0.25

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,6 @@
-// Full Astro Configuration API Documentation:
-// https://docs.astro.build/reference/configuration-reference
-
-// @type-check enabled!
-// VSCode and other TypeScript-enabled text editors will provide auto-completion,
-// helpful tooltips, and warnings if your exported object is invalid.
-// You can disable this by removing "@ts-check" and `@type` comments below.
+import react from '@astrojs/react';
 
 // @ts-check
 export default /** @type {import('astro').AstroUserConfig} */ ({
-  renderers: ['@astrojs/renderer-preact'],
+  integrations: [react()],
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,34 @@
 module.exports = {
-    content: [
-        './public/**/*.html',
-        './src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}'
-    ],
-    theme: {
-        extend: {
-            screens: {
-                'xs': "470px"
+  content: [
+    './public/**/*.html',
+    './src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}'
+  ],
+  theme: {
+    extend: {
+      screens: {
+        'xs': "470px"
+      },
+      fontFamily: {
+        'recoleta': ['Recoleta', 'ui-serif', 'Georgia', 'Cambria', "Times New Roman", 'Times', 'serif'],
+        'sans': ['IBM Plex Sans', 'ui-sans-serif', 'sans-serif']
+      },
+      typography: {
+        DEFAULT: {
+          css: {
+            'blockquote p:first-of-type::before': {
+              content: '',
             },
-            fontFamily: {
-                'recoleta': ['Recoleta', 'ui-serif', 'Georgia', 'Cambria', "Times New Roman", 'Times', 'serif'],
-                'sans': ['IBM Plex Sans', 'ui-sans-serif', 'sans-serif']
+            'blockquote p:last-of-type::after': {
+              content: '',
             },
-            typography: {
-                DEFAULT: {
-                    css: {
-                        'blockquote p:first-of-type::before': {
-                            content: '',
-                        },
-                        'blockquote p:last-of-type::after': {
-                            content: '',
-                        },
-                    }
-                }
-            }
-        },
+          }
+        }
+      }
     },
-    plugins: [
-        require('@tailwindcss/typography'),
-        require('@tailwindcss/aspect-ratio'),
-        require('@tailwindcss/line-clamp'),
-    ]
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/line-clamp'),
+  ]
 };
