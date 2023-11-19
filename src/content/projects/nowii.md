3044deffff4263ae0f6d6e1e137e03584b2ca220
nmanumr <nmanumr@gmail.com>
2023-11-19 17:33:47 +0500

    Update compareByDate function and edit project content
    Refactored compareByDate function in compareDates.ts to accommodate entries that might lack 'endDate'. This ensures a seamless comparison between different company and project entries and prevents potential errors.
    
    Also updated the content of nowii.md in the Projects section. Removed the 'draft' status, marked it as 'featured' and provided a description for better understanding of the project.

--- src/content/projects/nowii.md
+++ src/content/projects/nowii.md
@@ -1,10 +1,11 @@
 ---
 title: Nowii Rewards
 slug: nowii
-draft: true
 startDate: 2023-06-01
 endDate: 2023-08-01
 company: cb
+featured: true
 ---
 
-Content Missing
+A mobile app encouraging eco-friendly actions and rewarding those dedicated to environmental care. It celebrates
+and incentivizes individuals and organizations committed to sustainability.
--- src/utils/compareDates.ts
+++ src/utils/compareDates.ts
@@ -1,5 +1,18 @@
 import type {CollectionEntry} from 'astro:content';
 
-export const compareByDate = (a: CollectionEntry<'companies' | 'projects'>, b: CollectionEntry<'companies' | 'projects'>) =>
-  +(b.data.endDate ?? b.data.startDate) -
-  +(a.data.endDate ?? a.data.startDate);
+export const compareByDate = (a: CollectionEntry<'companies' | 'projects'>, b: CollectionEntry<'companies' | 'projects'>) => {
+  if (!b.data.endDate && !a.data.endDate) {
+    return +b.data.startDate - +a.data.startDate;
+  }
+
+  if (!b.data.endDate) {
+    return 1;
+  }
+
+  if (!a.data.endDate) {
+    return -1;
+  }
+
+  return +b.data.endDate - +a.data.endDate;
+}
+
