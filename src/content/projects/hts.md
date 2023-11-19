46c0e20282b14ace8255c7c8e4cb7586c2336bdf
nmanumr <nmanumr@gmail.com>
2023-11-19 16:09:49 +0500

    Refactor website UI and update content description
    Deleted 'objex.md' file as the associated project ended and is no longer relevant. Updated 'hts.md' to include a brief description of the project. Added an accent color in 'global.css' for better UI/UX and enhanced hover effects for anchor tags. Altered company details anchor tag in 'index.astro' to enhance user experience. Added colors library in 'tailwind.config.ts' to expand color options. These changes improve website's aesthetics and user experience while keeping the contents up-to-date.

--- src/content/projects/hts.md
+++ src/content/projects/hts.md
@@ -3,7 +3,7 @@ title: Hourtime Sheet
 slug: hts
 startDate: 2023-10-09
 company: cc
-draft: true
 ---
 
-Content Missing
+A complete timesheet software solution that allows employees and contractors to track work time and leave time with a
+simple user interface.
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,11 @@
-import typography from "@tailwindcss/typography";
-import type { Config } from 'tailwindcss'
+import typography from '@tailwindcss/typography';
+import colors from 'tailwindcss/colors';
+import {type Config} from 'tailwindcss';
 
 export default {
   content: [
     './public/**/*.html',
-    './src/**/*.{astro,js,jsx,svelte,ts,tsx,vue,md,mdx}'
+    './src/**/*.{astro,js,jsx,svelte,ts,tsx,vue,md,mdx}',
   ],
   theme: {
     colors: {
@@ -39,11 +40,11 @@ export default {
     },
     extend: {
       screens: {
-        'xs': "470px"
+        'xs': '470px',
       },
       fontFamily: {
-        'recoleta': ['Recoleta', 'ui-serif', 'Georgia', 'Cambria', "Times New Roman", 'Times', 'serif'],
-        'sans': ['IBM Plex Sans', 'ui-sans-serif', 'sans-serif']
+        'recoleta': ['Recoleta', 'ui-serif', 'Georgia', 'Cambria', 'Times New Roman', 'Times', 'serif'],
+        'sans': ['IBM Plex Sans', 'ui-sans-serif', 'sans-serif'],
       },
       typography: {
         DEFAULT: {
@@ -69,12 +70,12 @@ export default {
             'blockquote p:last-of-type::after': {
               content: '',
             },
-          }
-        }
-      }
+          },
+        },
+      },
     },
   },
   plugins: [
-    typography
-  ]
+    typography,
+  ],
 } satisfies Config;
